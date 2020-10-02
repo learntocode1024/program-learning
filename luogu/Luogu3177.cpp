c75ac60204d6676e1014ca8efe6552f3a1474106
learntocode1024 <learntocode1024@outlook.com>
2020-10-02 23:24:40 +0800

    AC on Oct 2,2020

--- luogu/Luogu3177.cpp
+++ luogu/Luogu3177.cpp
@@ -1,63 +1,44 @@
 // luogu/P3177.cpp
 // https://www.luogu.com.cn/problem/P3177
 // Created by learntocode1024 on 9/28/20.
-//
+// AC on Oct 2,2020
 
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
-#include <vector>
-#define MX 22
+#define MX 2002
 const int inf = 0x3fffffff;
-using std::vector;
 using std::max;
 using std::min;
 using std::memset;
 
-struct edge {
-  int to;
-  int val;
-};
-
-vector<edge> adj[MX];
+int head[MX];int t = 0;
+int val[MX<<1];
+int to[MX<<1];
+int next[MX<<1];
 int n, k;
 int size[MX];
-int dp[MX][MX][2]; // 2 spaces for colors
-bool vis[MX] = {false};
+long long dp[MX][MX], temp[MX];
 
-void dfs(int curr) {
-  vis[curr] = true;
+void dfs(const int &curr, const int & fa) {
+  dp[curr][0] = dp[curr][1] = 0;
   size[curr] = 1;
-  for (edge child : adj[curr]) {
-    if (vis[child.to]) continue;
-    dfs(child.to);
-    size[curr] += size[child.to];
+  for (int iter = head[curr]; iter; iter = next[iter]) {
+    if (to[iter] == fa) continue;
+    dfs(to[iter], curr);
     // dp
-    // black
-    dp[curr][1][1] += dp[child.to][0][0];
-    for (int bk = size[curr]; bk > 1; --bk) {
-      for (int kI = bk - 1; kI >= 0 ; --kI) {
-        if (kI > size[child.to]) continue;
-        if (kI != 0)
-          dp[curr][bk][1] = max(dp[curr][bk][1],
-                                dp[curr][bk - kI][1] + dp[child.to][kI][1] + child.val);
-        if (kI != size[child.to])
-          dp[curr][bk][1] = max(dp[curr][bk][1],
-                                dp[curr][bk - kI][1] + dp[child.to][kI][0]);
+    // learned from @czytysnow
+    // use special method to avoid superfluous calculates.
+    memset(temp, 0, sizeof(temp));
+    for (int bk = size[curr]; bk >= 0; --bk) {
+      for (int kI = 0; kI <= size[to[iter]]; ++kI) {
+        if (bk + kI > k) continue;
+        long long val_ = val[iter] * 1LL * ((k - kI)*kI + (n - k - size[to[iter]] + kI)*(size[to[iter]] - kI));
+        temp[bk + kI] = max(temp[bk + kI], dp[curr][bk] + dp[to[iter]][kI] + val_);
       }
     }
-    // white
-    for (int bk = size[curr] - 1; bk >= 0; --bk) {
-      for (int kI = bk; kI >= 0; --kI) {
-        if (kI > size[child.to]) continue;
-        if (kI != 0)
-          dp[curr][bk][0] = max(dp[curr][bk][0],
-                                dp[curr][bk - kI][0] + dp[child.to][kI][1]);
-        if (kI != size[child.to])
-          dp[curr][bk][0] = max(dp[curr][bk][0],
-                                dp[curr][bk - kI][0] + dp[child.to][kI][0] + child.val);
-      }
-    }
+    for(int j=0;j<=size[curr]+size[to[iter]];j++) dp[curr][j] = temp[j];
+    size[curr] += size[to[iter]];
   }
 }
 
@@ -65,22 +46,17 @@ int main() {
   scanf("%d%d", &n, &k);
   for (int kI = 1, a, b, c; kI < n; ++kI) {
     scanf("%d%d%d", &a, &b, &c);
-    adj[a].push_back({b, c});
-    adj[b].push_back({a, c});
+    val[++t] = c;
+    to[t] = b;
+    next[t] = head[a];
+    head[a] = t;
+    val[++t] = c;
+    to[t] = a;
+    next[t] = head[b];
+    head[b] = t;
   }
-  for (auto & kI : dp) {
-    kI[0][1] = -inf;
-  }
-  dfs(1);
-  for (int kI = 1; kI <= n; ++kI) {
-    printf("%d:\n", kI);
-    for (int kJ = 0; kJ <= n; ++kJ)
-      printf("(%d)%d ",kJ, dp[kI][kJ][0]);
-    printf("\n");
-    for (int kJ = 0; kJ <= n; ++kJ)
-      printf("(%d)%d ",kJ, dp[kI][kJ][1]);
-    printf("\n==========================\n");
-  }
-  printf("%d", max(dp[1][k][0], dp[1][k][1]));
+  std::fill(dp[0], dp[0] + MX * MX, -1);
+  dfs(1, 0);
+  printf("%lld", dp[1][k]);
   return 0;
 }
