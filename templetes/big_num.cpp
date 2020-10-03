d9936a495acc779572d9624583e2d22ae3816503
learntocode1024 <learntocode1024@outlook.com>
2020-10-03 23:40:45 +0800

    Edited on Oct 3 2020

---
+++ templetes/big_num.cpp
@@ -0,0 +1,5 @@
+//
+// Created by acgtech on 10/3/20.
+//
+
+#include "big_num.h"
---
+++ templetes/big_num.h
@@ -0,0 +1,12 @@
+//
+// Created by acgtech on 10/3/20.
+//
+
+#ifndef PROGRAM_LEARNING_TEMPLETES_BIG_NUM_H_
+#define PROGRAM_LEARNING_TEMPLETES_BIG_NUM_H_
+
+class big_num {
+
+};
+
+#endif //PROGRAM_LEARNING_TEMPLETES_BIG_NUM_H_
