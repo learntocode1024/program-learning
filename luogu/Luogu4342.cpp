a4d6dd858d24d8aa09b6a680d7204308b2954380
learntocode1024 <learntocode1024@outlook.com>
2020-10-16 23:55:59 +0800

    10/16/20

--- luogu/Luogu4342.cpp
+++ luogu/Luogu4342.cpp
@@ -1,23 +1,31 @@
 // luogu/Luogu4342.cpp
 // https://www.luogu.com.cn/problem/P4342
 // Created by learntocode1024 on 10/16/20.
-// 
+// AC on 10/16/20
 #include <iostream>
 #define MX 51
-#define DEBUG
 using std::cin;
 using std::cout;
 const int inf = 0x3f3f3f3f;
 int n;
 bool op[MX << 1] = {false};
-int dp[MX << 1][MX];
+int dp_max[MX << 1][MX];
+int dp_min[MX << 1][MX];
+
+inline void to_max(int &a, const int &b) {
+  if (a < b) a = b;
+}
+inline void to_min(int &a, const int &b) {
+  if (a > b) a = b;
+}
 
 int main() {
   // input
   cin >> n;
   for (int kI = 1; kI <= (n << 1); ++kI) {
     for (int kJ = 1; kJ <= n; ++kJ) {
-      dp[kI][kJ] = -inf;
+      dp_max[kI][kJ] = -inf;
+      dp_min[kI][kJ] = inf;
     }
   }
   {
@@ -25,40 +33,43 @@ int main() {
     char tmp_op;
     for (int kI = 1; kI <= n; ++kI) {
       cin >> tmp_op >> tmp_num;
-      dp[kI][1] = tmp_num;
-      dp[kI + n][1] = tmp_num;
+      dp_max[kI][1] = tmp_num;
+      dp_max[kI + n][1] = tmp_num;
+      dp_min[kI][1] = tmp_num;
+      dp_min[kI + n][1] = tmp_num;
       if (tmp_op == 't') {
         op[kI] = true;
         op[kI + n] = true;
       }
     }
   }
-  // dp
+  // dp_max
   for (int step = 2; step <= n; ++step) {
     for (int beg = 1; beg + step <= (n << 1); ++beg) {
       for (int kI = 1; kI < step; ++kI) {
-        int curr = dp[beg][kI];
-        if (op[beg + kI]) curr += dp[beg + kI][step - kI];
-        else curr *= dp[beg + kI][step - kI];
-        if (curr > dp[beg][step]) dp[beg][step] = curr;
+        if (op[beg + kI]) {
+          to_max(dp_max[beg][step], dp_max[beg][kI] + dp_max[beg + kI][step - kI]);
+          to_min(dp_min[beg][step], dp_min[beg][kI] + dp_min[beg + kI][step - kI]);
+        } else {
+          to_max(dp_max[beg][step], dp_max[beg][kI] * dp_max[beg + kI][step - kI]);
+          to_max(dp_max[beg][step], dp_min[beg][kI] * dp_min[beg + kI][step - kI]);
+          to_max(dp_max[beg][step], dp_max[beg][kI] * dp_min[beg + kI][step - kI]);
+          to_max(dp_max[beg][step], dp_min[beg][kI] * dp_max[beg + kI][step - kI]);
+          to_min(dp_min[beg][step], dp_min[beg][kI] * dp_min[beg + kI][step - kI]);
+          to_min(dp_min[beg][step], dp_max[beg][kI] * dp_max[beg + kI][step - kI]);
+          to_min(dp_min[beg][step], dp_min[beg][kI] * dp_max[beg + kI][step - kI]);
+          to_min(dp_min[beg][step], dp_max[beg][kI] * dp_min[beg + kI][step - kI]);
+        }
       }
     }
   }
-#ifdef DEBUG
-  for (int kI = 1; kI <= 2 * n; ++kI) {
-    for (int kJ = 1; kJ <= n; ++kJ) {
-      printf("%d ", dp[kI][kJ]);
-    }
-    printf("\n");
-  }
-#endif
   int max = 0;
   for (int kI = 1; kI <= n; ++kI) {
-    if (dp[kI][n] > max) max = dp[kI][n];
+    if (dp_max[kI][n] > max) max = dp_max[kI][n];
   }
   printf("%d\n", max);
   for (int kI = 1; kI <= n; ++kI) {
-    if (dp[kI][n] == max) printf("%d ", kI);
+    if (dp_max[kI][n] == max) printf("%d ", kI);
   }
   return 0;
 }
