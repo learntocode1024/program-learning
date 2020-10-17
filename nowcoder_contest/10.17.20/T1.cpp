44c94fc3f45509e14954ccfc9666394cc93efc25
learntocode1024 <learntocode1024@outlook.com>
2020-10-18 00:49:33 +0800

    10/18/20

---
+++ nowcoder_contest/10.17.20/T1.cpp
@@ -0,0 +1,42 @@
+// nowcoder_contest/101720/T1.cpp
+// https://ac.nowcoder.com/acm/contest/7605/A
+// Created by learntocode1024 on 10/17/20.
+// AC on 10/18/20
+#include <cstdio>
+#define N 3
+long long a[N], d, tmp;
+int cnt, tot;
+
+long long gcd(long long x, long long y) {
+  if (y)
+    while((x %= y) && (y %= x));
+  return x + y;
+}
+
+int main() {
+  scanf("%d", &cnt);
+  while (cnt--) {
+    tot = 0;
+    for (int kI = 0; kI < N; ++kI) {
+      scanf("%lld",&tmp);
+      if (tmp) a[tot++] = tmp;
+    }
+    scanf("%lld",&d);
+    if (tot == 0) {
+      if (d) printf("NO\n");
+      else printf("YES\n");
+      continue;
+    }
+    long long mod = a[0];
+    if (tot > 1) {
+      for (int kI = 1; kI < tot; ++kI)
+        mod = gcd(mod, a[kI]);
+    }
+    if (d % mod) {
+      printf("NO\n");
+    } else {
+      printf("YES\n");
+    }
+  }
+  return 0;
+}
