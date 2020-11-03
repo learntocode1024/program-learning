e94928a74d8ffe0f885fa16e1d18992b05d49aac
learntocode1024 <learntocode1024@outlook.com>
2020-11-03 21:02:31 +0800

    11/3/20

---
+++ CodeForces/CF896/B.cpp
@@ -0,0 +1,9 @@
+// CodeForces/CF896/B.cpp
+// https://codeforces.com/contest/896/problem/B
+// Created by learntocode1024 on 11/03/20.
+// 
+
+int main() {
+  
+  return 0;
+}
---
+++ CodeForces/CF896/D.cpp
@@ -0,0 +1,9 @@
+// CodeForces/CF896/D.cpp
+// https://codeforces.com/contest/896/problem/D
+// Created by learntocode1024 on 11/03/20.
+// 
+
+int main() {
+  
+  return 0;
+}
