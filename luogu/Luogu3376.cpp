d24234d1c2eb4254a8bfac3f5eae937b05cf2356
learntocode1024 <learntocode1024@outlook.com>
2020-11-23 23:40:56 +0800

    11/23/20

--- luogu/Luogu3376.cpp
+++ luogu/Luogu3376.cpp
@@ -1,12 +1,15 @@
 // luogu/Luogu3376.cpp
 // https://www.luogu.com.cn/problem/P3376
-// Created by learntocode1024 on 11/22/20.
+// Created by learntocode1024 on 11/23/20.
 // 
 
-class 
+#include <cstdio>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
   
   return 0;
 }
-
