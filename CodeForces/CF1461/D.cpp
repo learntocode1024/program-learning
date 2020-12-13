45a7af4dd52a340ffbe3d7bac7d286a694884520
learntocode1024 <learntocode1024@outlook.com>
2020-12-13 18:22:52 +0800

    12-13-20

--- CodeForces/CF1461/D.cpp
+++ CodeForces/CF1461/D.cpp
@@ -2,23 +2,68 @@
 // Author: learntocode1024
 // Date: 12-11-20
 // URL: https://codeforces.com/contest/1461/problem/D
-// tag:
-// 
+// tag: easy bruteforce
+// AC
 
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 #include <iostream>
-#define MX
+#include <set>
+#define MX 100005
 using std::cin;
 using std::cout;
 using std::endl;
 using std::memset;
 typedef long long LL;
 typedef unsigned long long ULL;
-const int mod = 1e9 + 7;
+
+ULL a[MX];
+std::set<ULL> mem;
+
+int getmid(int l, int r) {
+  if (l >= r) return -1;
+  int mid, key = (a[l] + a[r - 1]) >> 1;
+  while (r > l) {
+    mid = l + (r - l) / 2;
+    if (a[mid] <= key) l = mid + 1;
+    else r = mid;
+  }
+  return l;
+}
+
+ULL calc(int l, int r) {
+  ULL val;
+  if (a[l] == a[r - 1]) {
+    val = 1ull * a[l] * (r - l);
+    mem.insert(val);
+    return val;
+  }
+  int mid = getmid(l, r);
+  val = calc(l, mid) + calc(mid, r);
+  mem.insert(val);
+  return val;
+}
 
 void solve() {
-  
+  mem = std::set<ULL>();
+  memset(a, 0, sizeof(a));
+  int n, q;
+  cin >> n >> q;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  std::sort(a, a + n);
+  calc(0, n);
+  int t;
+  while (q--) {
+    cin >> t;
+    if (mem.find(t) != mem.end()) {
+      cout << "Yes\n";
+    } else {
+      cout << "No\n";
+    }
+  }
 }
 
 int main() {
