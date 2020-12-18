a8534915d38c6f2726b0570654f049ca4575d903
learntocode1024 <misaka18931@outlook.com>
2020-12-18 15:32:46 +0800

    12-18-20

--- CodeForces/CF1454/C.cpp
+++ CodeForces/CF1454/C.cpp
@@ -1,16 +1,47 @@
 // CodeForces/CF1454/C.cpp
 // https://codeforces.com/contest/1454/problem/C
 // Created by learntocode1024 on 11/24/20.
-// 
+// AC
 
 #include <cstdio>
+#include <algorithm>
 #include <iostream>
-#include <memory.h>
 using std::cin;
 using std::cout;
 using std::endl;
 
+int cnt[200005];
+bool vis[200005];
+
+void solve() {
+  int n;
+  cin >> n;
+  int prev = 0;
+  for (int i = 0, curr; i < n; ++i) {
+    cin >> curr;
+    vis[curr] = true;
+    if (curr == prev) continue;
+    if (i != 0) cnt[curr]++;
+    prev = curr;
+  }
+  cnt[prev]--;
+  int ans = n + 1;
+  for (int i = 1; i <= n; ++i) {
+    if (vis[i]) ans = std::min(ans, cnt[i] + 1);
+  }
+  cout << ans << endl;
+  for (int i = 1; i <= n; ++i) {
+    cnt[i] = 0;
+    vis[i] = false;
+  }
+}
+
 int main() {
-  
+  int T;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+  cout << endl;
   return 0;
 }
