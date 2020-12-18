048cf4dd9d5cb19c13758cf617e936a0f95c9eca
learntocode1024 <misaka18931@outlook.com>
2020-12-18 15:06:10 +0800

    12-18-20

--- CodeForces/CF1462/F.cpp
+++ CodeForces/CF1462/F.cpp
@@ -2,23 +2,42 @@
 // Author: learntocode1024
 // Date: 12-16-20
 // URL: https://codeforces.com/contest/1462/problem/F
-// tag: 
-// 
+// tag: index, binary_search
+// AC
 
 #include <cstdio>
-#include <cstring>
 #include <iostream>
-#define MX
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+#define MX 200005
 using std::cin;
 using std::cout;
 using std::endl;
-using std::memset;
-typedef long long LL;
-typedef unsigned long long ULL;
-const int mod = 1e9 + 7;
+std::vector<int> lt, rt;
+std::pair<int, int> seg[MX];
 
 void solve() {
-  
+  int n;
+  lt = std::vector<int>();
+  rt = std::vector<int>();
+  cin >> n;
+  for (int i = 0, l, r; i < n; ++i) {
+    cin >> l >> r;
+    seg[i] = std::make_pair(l, r);
+    lt.push_back(l);
+    rt.push_back(r);
+  }
+  std::sort(lt.begin(), lt.end());
+  std::sort(rt.begin(), rt.end());
+  int ans = n + 1;
+  for (int i = 0; i < n; ++i) {
+    int curr = std::lower_bound(rt.begin(), rt.end(), seg[i].first) - rt.begin();
+    curr += lt.end() - std::upper_bound(lt.begin(), lt.end(), seg[i].second);
+    ans = std::min(ans, curr);
+  }
+  cout << ans << endl;
 }
 
 int main() {
