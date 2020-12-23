f69905990dae387ff91cb456b2775cc9e9b63f11
learntocode1024 <misaka18931@outlook.com>
2020-12-23 17:20:12 +0800

    12-23-20

--- Codeforces/CF1463/F.cpp
+++ Codeforces/CF1463/F.cpp
@@ -6,26 +6,55 @@
 // 
 
 #include <cstdio>
-#include <cstring>
 #include <iostream>
-#define MX
+#define MX 300005
 using std::cin;
 using std::cout;
 using std::endl;
-using std::memset;
 typedef long long LL;
 typedef unsigned long long ULL;
-const int mod = 1e9 + 7;
+
+int prev[MX];
+int belong[MX];
+int cond[MX];
+
+int get(int x) {
+  if (belong[x] == x) return x;
+  belong[x] = get(belong[x]);
+  return belong[x];
+}
 
 void solve() {
-  
+  int n, k;
+  cin >> n >> k;
+  for (int i = 1; i <= n; ++i) {
+    cin >> prev[i];
+    belong[i] = (prev[i] == 0)? i: prev[i];
+  }
+  for (int i = 0, a, b; i < k; ++i) {
+    cin >> a >> b;
+    if (cond[b] || cond[a] == b) {
+      cout << "0" << endl;
+      return;
+    }
+    cond[b] = a;
+  } // input
+  for (int i = 1; i <= n; ++i) {
+    if (cond[i]) {
+      if (get(i) == get(cond[i]) && prev[i] != cond[i]) {
+        cout << "0" << endl;
+        return;
+      }
+    }
+  } // check
+  return;
 }
 
 int main() {
   cin.tie(NULL);
   std::ios::sync_with_stdio(false);
   int T = 1;
-  cin >> T;
+  // cin >> T;
   while (T--) {
     solve();
   }
